fb5d5d165c28b2cbe19a620ed4616143e3ab9289
Robin Wilborn <robin.wilborn@yahoo.com>
2016-03-07 15:52:31 -0800

    Renamed TicTacToe

--- src/boardgames/TicTacToe.java
+++ src/boardgames/TicTacToe.java
@@ -2,7 +2,7 @@ package boardgames;
 
 import java.util.Scanner;
 
-public class tictactoe {
+public class TicTacToe {
   
    public static final int EMPTY = 0;
    public static final int CROSS = 1;
