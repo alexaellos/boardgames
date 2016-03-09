a21427db877e1b7d0292a1521c4747f7f553992b
Alfonso Almazan Ruelas <alfonso@promenadesoftware.com>
2016-03-09 15:49:22 -0800

    Update on client. Not complete thou

--- src/boardgames/client.java
+++ src/boardgames/client.java
@@ -1,23 +1,97 @@
 package boardgames;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ *
+ *  Client -> Server           Server -> Client
+ *  ----------------           ----------------
+ *  MOVE <n>  (0 <= n <= x)    WELCOME <char> 
+ *  QUIT                       VALID_MOVE
+ *                             OTHER_PLAYER_MOVED <n>
+ *                             VICTORY
+ *                             DEFEAT
+ *                             TIE
+ *                             MESSAGE <text>
+ *
+ */
 public class client {
-	GameBoard instance;
-	String playerId;
-	
-	
-	public client(){
-		
-	}
-	
-	private void update(){
-		
-	}
-	
-	private void getSpriteById(){
-		
-	}
-	
-	private void sendCommand(){
-		
-	}
+
+    private static int PORT = 8901;
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+    private GameBoard instance;
+//    private Player player;
+
+    /**
+     * Constructs the client by connecting to a server
+     */
+    public client(String serverAddress) throws Exception {
+        // Setup networking
+     	
+        socket = new Socket(serverAddress, PORT);
+        in = new BufferedReader(new InputStreamReader(
+            socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+    }
+    
+
+    /**
+     * The main thread of the client will listen for messages
+     * from the server.  The first message will be a "WELCOME"
+     * message in which we receive our mark.  Then we go into a
+     * loop listening for "VALID_MOVE", "OPPONENT_MOVED", "VICTORY",
+     * "DEFEAT", "TIE", "OPPONENT_QUIT or "MESSAGE" messages,
+     * and handling each message appropriately.  The "VICTORY",
+     * "DEFEAT" and "TIE" ask the user whether or not to play
+     * another game.  If the answer is no, the loop is exited and
+     * the server is sent a "QUIT" message.  If an OPPONENT_QUIT
+     * message is received then the loop will exit and the server
+     * will be sent a "QUIT" message also.
+     */
+    public void play() throws Exception {
+        String response;
+        try {
+            response = in.readLine();
+            if (response.startsWith("WELCOME")) {
+                char mark = response.charAt(8);
+            }
+            while (true) {
+                response = in.readLine();
+                if (response.startsWith("VALID_MOVE")) {
+                } else if (response.startsWith("OPPONENT_MOVED")) {
+                    int loc = Integer.parseInt(response.substring(15));
+                } else if (response.startsWith("VICTORY")) {
+                    break;
+                } else if (response.startsWith("DEFEAT")) {
+                    break;
+                } else if (response.startsWith("TIE")) {
+                    break;
+                } else if (response.startsWith("MESSAGE")) {
+                }
+            }
+            out.println("QUIT");
+        }
+        finally {
+            socket.close();
+        }
+    }
+
+
+    /**
+     * Runs the client as an application.
+     */
+    public static void main(String[] args) throws Exception {
+        while (true) {
+            String serverAddress = (args.length == 0) ? "localhost" : args[1];
+            client client = new client(serverAddress);
+            GUI gui = new GUI();
+            gui.loadStartFrame();
+            client.play();
+        }
+    }
 }
