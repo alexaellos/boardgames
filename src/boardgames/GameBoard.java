29d60eebe3489c0e65072bd5b6e7546f434b93e5
kshirai <kshirai@uci.edu>
2016-03-12 21:44:56 -0800

    hi

--- src/boardgames/GameBoard.java
+++ src/boardgames/GameBoard.java
@@ -5,10 +5,7 @@ public interface GameBoard {
 	/* private ArrayList<ArrayList<Piece>> board; */
 	
 	public boolean commandIsValid (Command c);
-	public void setCurrentPlayer(String s);
-	public void setGameOver(boolean gameOver);
 	public String getCurrentPlayer();
-	public boolean isGameOver();
 	public gameStatus getGameStatus();
 	public GameBoardGUI getGameBoardGUI();
 }
