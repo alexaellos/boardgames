1acf51f692f5e150a94a9b2005f0770411562702
Alex Aellos <aaellos@uci.edu>
2016-03-12 22:04:47 -0800

    Changed nested Arraylist to 2d Array

--- src/boardgames/GUIUpdater.java
+++ src/boardgames/GUIUpdater.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public interface GUIUpdater {
 	public String startGUI(ArrayList<String> gameList);		// Replace with Game[Board]GUI later
 	public String loadGameBoardGUI(GameBoardGUI game);
-	public void updateGameBoardGUI(String playerName, ArrayList<ArrayList<Piece>> board);
+	public void updateGameBoardGUI(String playerName, Piece[][] board);
 	public String getGUIChange();
+	public String loadWaitingGUI();
 }
