1d9db7fae9f6a2adef811329e8383653b3d425ab
Evelyn Luu <eyluu@uci.edu>
2016-03-14 23:45:14 -0700

    Added more comments to GUIController to describe how GUI and client can currently interact

--- src/GUI/GUIController.java
+++ src/GUI/GUIController.java
@@ -13,18 +13,27 @@ public class GUIController {
 		gui.startGUI();
 		
 		while(true){
+			// The player decided to exit out of the name input dialog,
+			// which means they decided to exit the program.
 			gameStatus status = gui.getStatus();
 			if(status == gameStatus.exit){
 				break;
 			}
+			// The program is still waiting for the user to:
+			// (1) Enter their name
+			// (2) Select a game
 			else if(status == gameStatus.loadingPlayer){
 				System.out.println("loading");
 			}
+			// The player has selected a game and has reached the loading screen.
+			// They are now waiting to be matched to another player.
 			else if(status == gameStatus.gameSelected){
 				Games game = gui.getSelectedGame();
 				GameBoard gameboard = game.getGameBoard();
 				
 				// This doesn't work for Tic Tac Toe but it works for Othello & Checkers.
+				// It won't work until Tic Tac Toe instantiates itself and matches the structure
+				// of the other game plug-ins.
 				System.out.println("Selected game: " + gameboard.getGameBoardGUI().getTitle());
 			}
 			else{
